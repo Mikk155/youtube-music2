4cf1cfe4970cf68f2be43541e261f485196f7762
Mikk155 <martimsanchez155@gmail.com>
2025-07-20 12:01:25 -0300

    Startup with the program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using YoutubeDLSharp;
+using YoutubeDLSharp.Options;
+using Spectre.Console;
+
+class Program
+{
+    public static string Workspace =>
+        Directory.GetCurrentDirectory();
+
+    public static string MusicFolder =>
+        Path.Combine( Workspace, "music" );
+
+    public static void HasExtension( string executable )
+    {
+        if( !File.Exists( Path.Combine( Workspace, $"{executable}.exe" ) ) )
+        {
+            Console.WriteLine( $"ERROR: {executable}.exe was not found at {Workspace}" );
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+
+    static async Task Main( string[] args )
+    {
+        HasExtension( "yt-dlp" );
+        HasExtension( "ffmpeg" );
+    }
+}
